8b780c1bb690d7df6811f37b6db015b711bef2a4
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-04-21 17:03:27 +0200

    edited signup validations to not validate empty fields

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,47 +5,43 @@ import asyncHandler from "express-async-handler";
 
 // Validation rules for sign-up
 export const validateSignUp = [
-    body('name').
-    notEmpty().
-    withMessage('Name is required')
-    ,
-
-    body('email')
-    .notEmpty().withMessage('Email is required')
-    .if((value, { req }) => value !== '') // Check if the field is not empty
-    .trim() // Trim whitespace
-    .isEmail() // Check if it's a valid email
-    .withMessage('Email is invalid')
-    .custom(async (value, { req }) => {
-        if (value) {
-            const existingUser = await User.findOne({ email: value });
-            if (existingUser) {
-                throw new Error('Email already exists');
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .if((value, { req }) => value !== "") // Check if the field is not empty
+        .trim() // Trim whitespace
+        .isEmail() // Check if it's a valid email
+        .withMessage("Email is invalid")
+        .custom(async (value, { req }) => {
+            if (value) {
+                const existingUser = await User.findOne({ email: value });
+                if (existingUser) {
+                    throw new Error("Email already exists");
+                }
             }
-        }
-    }),
+        }),
 
-    body('password')
-    .notEmpty().withMessage('Password is required')
-    .if((value, { req }) => value !== '') // Check if the field is not empty
-    .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters'),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .if((value, { req }) => value !== "") // Check if the field is not empty
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters"),
 
-body('confirmPassword')
-    .notEmpty().withMessage('Password Confirmation is required')
-    .if((value, { req }) => value !== '') // Check if the field is not empty
-    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
-    .custom((value, { req }) => {
-        const { password } = req.body;
-        if (value !== password) {
-            throw new Error('Passwords do not match');
-        }
-        return true; // Indicates the validation passed
-    }),
-
-
-
-
+    body("confirmPassword")
+        .notEmpty()
+        .withMessage("Password Confirmation is required")
+        .if((value, { req }) => value !== "") // Check if the field is not empty
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters")
+        .custom((value, { req }) => {
+            const { password } = req.body;
+            if (value !== password) {
+                throw new Error("Passwords do not match");
+            }
+            return true; // Indicates the validation passed
+        }),
 ];
 export const signUp = asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
@@ -54,8 +50,8 @@ export const signUp = asyncHandler(async (req, res, next) => {
     }
 
     try {
-        const { name, email, password ,confirmPassword} = req.body;
-        console.log(name,email,confirmPassword,password)
+        const { name, email, password, confirmPassword } = req.body;
+        console.log(name, email, confirmPassword, password);
 
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -64,16 +60,16 @@ export const signUp = asyncHandler(async (req, res, next) => {
         const user = new User({
             name,
             email,
-            password: hashedPassword
+            password: hashedPassword,
         });
 
         // Save the user to the database
         const result = await user.save();
-        
+
         res.send("User created successfully");
     } catch (error) {
         // Handle errors
-        console.error('Error creating user:', error);
+        console.error("Error creating user:", error);
         return next(error);
     }
 });
