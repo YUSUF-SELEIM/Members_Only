b4f1a469e61357e15affdbc5691f41574e219841
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-04-24 22:40:58 +0200

    Add withCredentials option to login POST request in loginModal.tsx and that did solve the problem, the cookie was not sent in the first place

--- client/components/loginModal.tsx
+++ client/components/loginModal.tsx
@@ -24,6 +24,7 @@ export const Login = ({ setIsLoggedIn, isOpen, onOpenChange }: { setIsLoggedIn: 
           headers: {
             'Content-Type': 'application/json',
           },
+          withCredentials: true,
         }
       );
 
