57a0c05633924607bbd443c13bc949e906eba6f9
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-04-29 01:49:23 +0300

    Refactor Navbar component to include logout functionality

--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { Navbar as NextUINavbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, useDisclosure } from "@nextui-org/react";
 import NextLink from "next/link";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -6,14 +5,29 @@ import {
     GithubIcon,
 } from "@/components/icons";
 import { Logo } from "@/components/icons";
-import { Button } from "@nextui-org/button";
-import { Login } from "@/components/loginModal";
-import { Signup } from "@/components/signupModal";
+import axios from 'axios';
 
 
-export const Navbar = ({ setIsLoggedIn }: { setIsLoggedIn: (isLoggedIn: boolean) => void; }) => {
-    const { onOpen: onLoginOpen, isOpen: isLoginOpen, onOpenChange: onLoginOpenChange } = useDisclosure();
-    const { onOpen: onSignupOpen, isOpen: isSignupOpen, onOpenChange: onSignupOpenChange } = useDisclosure();
+export const Navbar = ({ userData, isLoggedIn }: { userData: any; isLoggedIn: boolean; }) => {
+    const handleLogout = async () => {
+        try {
+            // Make a POST request to logout endpoint
+            const response = await axios.post('http://localhost:3000/api/log-out', {
+                withCredentials: true,
+            });
+
+            // Check if the logout was successful
+            if (response.status === 200) {
+                console.log('Logout successful');
+                window.location.reload();
+            } else {
+                console.error('Logout failed:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error logging out:', error);
+        }
+    };
+
     return (
         <NextUINavbar className="justify-center w-full">
             <NavbarContent className="flex justify-between w-full gap-4">
@@ -22,51 +36,37 @@ export const Navbar = ({ setIsLoggedIn }: { setIsLoggedIn: (isLoggedIn: boolean)
                         <Logo />
                         <p className="font-bold text-inherit">MembersOnly</p>
                     </NextLink>
-                </NavbarBrand>
-
-
+                </NavbarBrand>  
                 <NavbarItem className="flex gap-2">
                     <Link isExternal href="" aria-label="Github">
                         <GithubIcon className="text-default-500" />
                     </Link>
                     <ThemeSwitch />
                 </NavbarItem>
-
-                <NavbarItem>
-                    <Button onPress={onLoginOpen} color="primary" variant="ghost">
-                        Login
-                    </Button>
-                    <Login setIsLoggedIn={setIsLoggedIn} isOpen={isLoginOpen} onOpenChange={onLoginOpenChange} />
-                </NavbarItem>
-                <NavbarItem>
-                    <Button onPress={onSignupOpen}  color="primary" variant="ghost">
-                        Sign up
-                    </Button>
-                    <Signup isOpen={isSignupOpen} onOpenChange={onSignupOpenChange} />
-                </NavbarItem>
-
-                <Dropdown placement="bottom-start">
-                    <DropdownTrigger>
-                        <Avatar
-                            isBordered
-                            as="button"
-                            className="transition-transform"
-                            color="secondary"
-                            name="Jason Hughes"
-                            size="sm"
-                            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                        />
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Profile Actions" variant="flat">
-                        <DropdownItem key="profile" className="gap-2 h-14">
-                            <p className="font-semibold">Signed in as</p>
-                            <p className="font-semibold">zoey@example.com</p>
-                        </DropdownItem>
-                        <DropdownItem key="logout" color="danger">
-                            Log Out
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
+                {isLoggedIn && (
+                    <Dropdown placement="bottom-start">
+                        <DropdownTrigger>
+                            <Avatar
+                                isBordered
+                                as="button"
+                                className="transition-transform"
+                                color="secondary"
+                                name="Jason Hughes"
+                                size="sm"
+                                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                            />
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Profile Actions" variant="flat">
+                            <DropdownItem key="profile" className="gap-2 h-14">
+                                <p className="font-semibold">Signed in as</p>
+                                <p className="font-semibold">{userData?.email}</p>
+                            </DropdownItem>
+                            <DropdownItem key="logout" color="danger" onClick={handleLogout}>
+                                Log Out
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                )}
             </NavbarContent>
         </NextUINavbar>
     );
