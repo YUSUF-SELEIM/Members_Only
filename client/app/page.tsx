866cac0303028f1e5f0d2567d0d482ba90c48a19
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-04-29 02:40:41 +0300

    messages and login/signup div are next to eah other and logging would hide that div

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,6 +3,9 @@ import Footer from "@/components/footer";
 import axios from 'axios';
 import { useEffect, useState } from "react";
 import { Navbar } from "@/components/navbar";
+import { Button, useDisclosure } from "@nextui-org/react";
+import { Login } from "@/components/loginModal";
+import { Signup } from "@/components/signupModal";
 
 export default function Home() {
 	const [userData, setUserData] = useState(null);
@@ -25,6 +28,7 @@ export default function Home() {
 				if (response.status === 200) {
 					// Extract the user data from the response and set it in state
 					setUserData(response.data);
+					setIsLoggedIn(true);
 					console.log('User data:', response.data);
 				} else {
 					// Handle unexpected response status
@@ -37,30 +41,47 @@ export default function Home() {
 		};
 
 		// Fetch user data when the component renders
-		fetchUserData();
+		// fetchUserData();
 
 		// Fetch user data again if the user is logged in
 		if (isLoggedIn) {
 			fetchUserData();
 		}
 	}, [isLoggedIn]); // Run this effect when the isLoggedIn state changes
+	const { onOpen: onLoginOpen, isOpen: isLoginOpen, onOpenChange: onLoginOpenChange } = useDisclosure();
+	const { onOpen: onSignupOpen, isOpen: isSignupOpen, onOpenChange: onSignupOpenChange } = useDisclosure();
 
 
 	return (
-		<>
-			<Navbar setIsLoggedIn={setIsLoggedIn} />
-			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<main className="flex flex-col items-center justify-center w-full gap-4 px-6">
-					<h1 className="text-4xl font-bold text-center">Welcome to MembersOnly</h1>
-					<p className="text-lg text-center">
-						An exclusive community for members only
-						<br />
-						{/* Display the user's name if available, otherwise, display 'Guest' */}
-						Welcome back, {userData?.name ?? 'Guest'}!
-					</p>
-				</main>
-				<Footer />
-			</section>
-		</>
+
+
+		<div className="flex flex-col items-center justify-center w-full h-full gap-4">
+			<Navbar userData={userData} isLoggedIn={isLoggedIn} />
+			<main className="flex flex-col items-center justify-center w-full h-full gap-4 md:flex-row ">
+				{!isLoggedIn && (
+					<div className="flex flex-col items-center justify-center h-[calc(100dvh)] w-full md:h-full mt-32 space-y- md:mt-0">
+						<h1 className="text-4xl font-bold text-center">Welcome to MembersOnly</h1>
+						<p className="text-lg text-center">
+							An exclusive community for members only
+							<br />
+						</p>
+						<div className="flex space-x-2">
+							<Button onPress={onLoginOpen} color="primary" variant="ghost">
+								Login
+							</Button>
+							<Button onPress={onSignupOpen} color="primary" variant="ghost">
+								Sign up
+							</Button>
+						</div>
+						<Login setIsLoggedIn={setIsLoggedIn} isOpen={isLoginOpen} onOpenChange={onLoginOpenChange} />
+						<Signup isOpen={isSignupOpen} onOpenChange={onSignupOpenChange} />
+					</div>
+				)}
+				<div className="flex flex-col items-center justify-between w-full h-[calc(100dvh)] md:h-full">
+					<h1 className="text-5xl">Messages</h1>
+					<Footer />
+				</div>
+			</main>
+		</div>
 	);
 }
