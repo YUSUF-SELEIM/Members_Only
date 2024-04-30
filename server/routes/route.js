6985ee91c4faeaa28fdad9a8e468d6f43fe34a7f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-04-30 06:40:58 +0300

    Refactor route.js to change cookie props and changed /logout method to delete now refreshing does not log the user out and logging out deletes the session cookies as i think

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { signUp , validateSignUp } from '../controllers/userController.js';
-import  passport from 'passport';
-import { Strategy as LocalStrategy} from 'passport-local';
+import { signUp, validateSignUp } from '../controllers/userController.js';
+import { savePost } from '../controllers/postController.js';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
 import session from 'express-session';
 import User from '../models/User.js';
 import { body, validationResult } from "express-validator";
@@ -13,72 +14,73 @@ router.use(
   session({
     secret: "cats",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: 'mongodb+srv://yusufabdelfattah207:xRcBV80rikJQvLaA@cluster0.jb173jl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' }),
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-      httpOnly: true, // Set to true for improved security
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      httpOnly: false, // Set to true for improved security
       secure: false, // Set to true in production for HTTPS-only
       sameSite: 'lax', // Set to 'lax' or 'strict' in production
     },
+
   })
 );
 
 router.use(passport.session());
 router.use(express.urlencoded({ extended: false }));
 
-router.post('/sign-up',validateSignUp ,signUp);
+router.post('/sign-up', validateSignUp, signUp);
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     console.log("Email is  ", username);
-    console.log("Password is  ", password); 
+    console.log("Password is  ", password);
     try {
       const user = await User.findOne({ email: username });
       console.log("User is  ", user);
       if (!user) {
-          return done(null, false, {
-            errors: [
-              { type: 'field', value: '', msg: 'Incorrect email', path: 'username', location: 'body' },
-              // You can include other error objects here if needed
-            ]
-          });
+        return done(null, false, {
+          errors: [
+            { type: 'field', value: '', msg: 'Incorrect email', path: 'username', location: 'body' },
+            // You can include other error objects here if needed
+          ]
+        });
       };
       const match = await bcrypt.compare(password, user.password);
-  
+
       if (!match) {
-          return done(null, false, {
-            errors: [
-              { type: 'field', value: '', msg: 'Incorrect password', path: 'password', location: 'body' },
-              // You can include other error objects here if needed
-            ]
-          });
+        return done(null, false, {
+          errors: [
+            { type: 'field', value: '', msg: 'Incorrect password', path: 'password', location: 'body' },
+            // You can include other error objects here if needed
+          ]
+        });
       };
       return done(null, user);
-      } catch(err) {
+    } catch (err) {
       return done(err);
-      };
-  
+    };
+
   })
-  );
+);
 
 // Validation rules for sign-up
 export const validateLogIn = [
   body("username")
-      .notEmpty()
-      .withMessage("Email is required")
-      .if((value, { req }) => value !== "") // Check if the field is not empty
-      .trim() // Trim whitespace
-      .isEmail() // Check if it's a valid email
-      .withMessage("Email is invalid")
-     ,
+    .notEmpty()
+    .withMessage("Email is required")
+    .if((value, { req }) => value !== "") // Check if the field is not empty
+    .trim() // Trim whitespace
+    .isEmail() // Check if it's a valid email
+    .withMessage("Email is invalid")
+  ,
 
   body("password")
-      .notEmpty()
-      .withMessage("Password is required")
-      .if((value, { req }) => value !== "") // Check if the field is not empty
-      .isLength({ min: 8 })
-      .withMessage("Password must be at least 8 characters"),
+    .notEmpty()
+    .withMessage("Password is required")
+    .if((value, { req }) => value !== "") // Check if the field is not empty
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
 
 ];
 
@@ -99,7 +101,7 @@ router.post('/log-in', validateLogIn, (req, res, next) => {
           // You can include other error objects here if needed
         ]
       });
-          }
+    }
     req.login(user, (err) => {
       if (err) {
         return next(err);
@@ -119,9 +121,9 @@ passport.deserializeUser(async (id, done) => {
   try {
     //extensive use of that
     const user = await User.findById(id);
-    console.log("deserialized",user);
+    console.log("deserialized", user);
     done(null, user);
-  } catch(err) {
+  } catch (err) {
     done(err);
   };
 });
@@ -142,7 +144,7 @@ router.get('/user', async (req, res) => {
     // If user data is not found, return a 400 response
     if (!user) {
       return res.status(400).json({ message: 'User not found' });
-    } 
+    }
 
     // Send the user data as a JSON response
     res.status(200).json(user);
@@ -153,25 +155,26 @@ router.get('/user', async (req, res) => {
   }
 });
 
-router.post("/log-out", (req, res, next) => {
+router.delete("/log-out", (req, res, next) => {
   console.log("Logging out");
   req.logout((err) => {
     if (err) {
       console.log(err);
       return next(err);
     }
-    
+
     // Clear the session cookie
-    res.clearCookie('connect.sid', { 
-    httpOnly: true, // Set to true for improved security
-    secure: false, // Set to true in production for HTTPS-only
-    sameSite: 'lax', // Set to 'lax' or 'strict' in production });
-  });
+    res.clearCookie('connect.sid', {
+      httpOnly: true, // Set to true for improved security
+      secure: false, // Set to true in production for HTTPS-only
+      sameSite: 'lax', // Set to 'lax' or 'strict' in production });
+    });
     // Respond with a success message or redirect to another page
     res.status(200).send({ message: 'Logout successful' });
   });
 });
 
 
+router.post('/new-post', savePost);
 
 export default router;
