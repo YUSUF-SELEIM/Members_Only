19123bbcf630e1ec26f7f02cc9a0a89ca5c03043
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-05-06 11:31:54 +0300

    changed favicon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 		{ media: "(prefers-color-scheme: dark)", color: "black" },
 	],
 	icons: {
-		icon: "/favicon.ico",
+		icon: "/group.png",
 		shortcut: "/favicon-16x16.png",
 		apple: "/apple-touch-icon.png",
 	},
