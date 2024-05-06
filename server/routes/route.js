0df35bde8a438d22838e4153c426a7728ca6410d
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-05-06 15:54:20 +0300

    Update session cookie settings to use 'strict' sameSite attribute

--- server/routes/route.js
+++ server/routes/route.js
@@ -18,7 +18,7 @@ router.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       secure: true, // Set to true in production for HTTPS-only
-      sameSite: 'none', 
+      sameSite: 'strict', 
     },
   })
 );
