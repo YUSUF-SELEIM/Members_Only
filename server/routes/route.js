ec8b3bdd70c40c5b43c96a227f666b6e8802359d
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-05-06 17:45:07 +0300

    Update session cookie settings to use 'lax' sameSite attribute for improved security

--- server/routes/route.js
+++ server/routes/route.js
@@ -18,7 +18,7 @@ router.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       secure: true, // Set to true in production for HTTPS-only
-      sameSite: 'none', // Set to 'lax' or 'strict' in production
+      sameSite: 'lax', // Set to 'lax' or 'strict' in production
       httpOnly: false, // Set to true for improved security
     },
   })
