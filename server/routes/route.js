6cea9939978aa3b13872a2bebd8edae531662def
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-05-06 15:48:28 +0300

    Update session cookie settings for improved security

--- server/routes/route.js
+++ server/routes/route.js
@@ -18,8 +18,8 @@ router.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       httpOnly: true, // Set to true for improved security
-      secure: false, // Set to true in production for HTTPS-only
-      sameSite: 'lax', // Set to 'lax' or 'strict' in production
+      secure: true, // Set to true in production for HTTPS-only
+      sameSite: 'none', 
     },
   })
 );
